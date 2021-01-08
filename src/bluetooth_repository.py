5b4d1ba7e60324ca17e74d5865dc2d468ac5a0a0
leonhead <qubit07@gmail.com>
2021-01-08 10:22:38 +0100

    Update bluetooth_repository.py

--- src/bluetooth_repository.py
+++ src/bluetooth_repository.py
@@ -4,12 +4,12 @@ class BluetoothRepository:
   URI = "mongodb://mongodb:27017"
   DATABASE = None
 
-@staticmethod
-def initialize():
-  client = MongoClient(BluetoothRepository.URI)
-  BluetoothRepository.DATABASE = client["bluetooth"]
+  @staticmethod
+  def initialize():
+    client = MongoClient(BluetoothRepository.URI)
+    BluetoothRepository.DATABASE = client["bluetooth"]
 
 
-@staticmethod
-def insert(collection, data):
-  return BluetoothRepository.DATABASE[collection].insert(data)
+  @staticmethod
+  def insert(collection, data):
+    return BluetoothRepository.DATABASE[collection].insert(data)
